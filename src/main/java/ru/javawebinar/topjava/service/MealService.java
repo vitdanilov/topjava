48fd6fab7604b52b781d28e920687b7eb68cfda5
Vitalii.Danilov <Vitalii.Danilov@nexign.com>
2020-02-18 20:58:24 +0300

    Layers 1

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,36 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 public class MealService {
 
     private MealRepository repository;
+
+    public List<Meal> getAll() {
+        return repository.getAll().stream().collect(Collectors.toList());
+    }
+
+    public Meal get(int id) {
+        return checkNotFoundWithId(repository.get(id), id);
+    }
+
+    public Meal create(Meal meal) {
+        return repository.save(meal);
+    }
+
+    public void delete(int id) {
+        checkNotFoundWithId(repository.delete(id), id);
+    }
+
+    public void update(Meal meal) {
+        checkNotFoundWithId(repository.save(meal), meal.getId());
+    }
 
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,35 @@
 package ru.javawebinar.topjava.web.meal;
 
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
-public class MealRestController {
+import java.util.List;
+
+public class MealRestController extends AbstractMealController {
     private MealService service;
 
+    @Override
+    public List<Meal> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    public Meal get(int id) {
+        return super.get(id);
+    }
+
+    @Override
+    public Meal create(Meal Meal) {
+        return super.create(Meal);
+    }
+
+    @Override
+    public void delete(int id) {
+        super.delete(id);
+    }
+
+    @Override
+    public void update(Meal Meal, int id) {
+        super.update(Meal, id);
+    }
 }
