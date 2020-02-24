b500ab07ae3aec150fe052b332b97824fd9ea875
Vitalii.Danilov <Vitalii.Danilov@nexign.com>
2020-02-24 11:07:49 +0300

    create and init table meal

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,21 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+  ('User', 'user1@yandex.ru', 'password'),
+  ('Admin', 'admi1@gmail.com', 'admin');
 
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories) VALUES
+((select id from users where name='User'), now(),'обед1', 1500);
+
+INSERT INTO meals (user_id, date_time, description, calories) VALUES
+((select id from users where name='User'), now(),'омлет1', 1000);
+
+INSERT INTO meals (user_id, date_time, description, calories) VALUES
+((select id from users where name='Admin'), now(),'админский омлет1', 1000);
